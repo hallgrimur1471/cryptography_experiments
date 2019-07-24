0970d00a890b6e608755e0245f2b030bfd5362e8
hallgrimur1471 <hallgrimur1471@gmail.com>
2019-07-24 00:04:56 +0000

    Lib setup

--- setup.py
+++ setup.py
@@ -1,15 +1,5 @@
 import setuptools
 
-# setuptools.setup(
-#    name="cryptography_experiments2",
-#    packages=["cryptography_experiments2"],
-#    package_dir={"cryptography_experiments2": "lib"},
-#    version="1.0",
-#    install_requires=["pycryptodome"],
-# )
 setuptools.setup(
-    name="foobar",
-    version="1.0",
-    package_dir={"foobar": "lib"},
-    packages=["foobar"],
+    name="cryptolib", version="1.0", install_requires=["pycryptodome"]
 )
