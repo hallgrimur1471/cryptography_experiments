f02fb6e89f718579251a0bb8680b84c218a83b1e
hallgrimur1471 <hallgrimur1471@gmail.com>
2020-06-01 21:10:22 +0000

    Update challenge 4 after changing package setup

--- src/drvn/cryptography_challenges/s01_c04_detect_single_character_xor.py
+++ src/drvn/cryptography_challenges/s01_c04_detect_single_character_xor.py
@@ -1,2 +1,27 @@
+# pylint:disable=unnecessary-lambda
+"""
+Finds line in file that has been encrypted using single-character XOR
+"""
+import logging
+
+import drvn.cryptography.xor as xor
+import drvn.cryptography_challenges._resources as resources
+
+
 def run_challenge():
-    print("Running challenge 1.4")
+    logging.info("Running challenge 4 ...")
+
+    data = resources.get_contents("c04_detect_single_character_xor.in")
+    data = [line.rstrip() for line in data.split("\n")]
+    data = [bytes.fromhex(line) for line in data]
+
+    decrypted_lines = []
+    for i, line in enumerate(data):
+        logging.info("Analysing line {}/{}".format(i + 1, len(data)))
+        decrypted = xor.single_byte_decryption(line)
+        decrypted = decrypted[0]
+        decrypted_lines.append(decrypted)
+    decrypted_lines.sort(key=lambda m: m.frequency_distance)
+    most_probable = decrypted_lines[0]
+    logging.info(f"data: {most_probable.data}")
+    logging.info(f"key: {chr(most_probable.key)}")
