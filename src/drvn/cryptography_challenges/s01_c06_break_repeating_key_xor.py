1c9d94179ea343fa5b0b0fdd441fbb96e7e3cdd1
hallgrimur1471 <hallgrimur1471@gmail.com>
2020-06-01 21:47:12 +0000

    Update challenge 6 after changing package setup

--- src/drvn/cryptography_challenges/s01_c06_break_repeating_key_xor.py
+++ src/drvn/cryptography_challenges/s01_c06_break_repeating_key_xor.py
@@ -1,2 +1,24 @@
+# pylint:disable=unnecessary-lambda
+"""
+Break repeating-key XOR test
+"""
+
+import base64
+from functools import reduce
+import logging
+
+import drvn.cryptography.xor as xor
+import drvn.cryptography_challenges._resources as resources
+
+
 def run_challenge():
-    print("Running challenge 1.6")
+    logging.info("Running challenge 6 ...")
+
+    data = resources.get_contents("c06_break_repeating_key_xor.in")
+    cipher = list(map(lambda line: line.rstrip(), data.split("\n")))
+    cipher = list(map(lambda line: base64.b64decode(line), cipher))
+    cipher = reduce(lambda acc, elem: acc + elem, cipher, bytearray())
+
+    decryptionResult = xor.decrypt(cipher)
+    logging.info(f"********* DATA ********\n{decryptionResult.data.decode()}")
+    logging.info(f"********* KEY ********\n{decryptionResult.key.decode()}")
