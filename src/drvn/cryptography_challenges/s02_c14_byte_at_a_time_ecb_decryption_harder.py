5ff3174fffa8ed6b74bc7e98ca2b4eb884786bd5
hallgrimur1471 <hallgrimur1471@gmail.com>
2020-06-16 22:58:11 +0000

    Add challenge 14

---
+++ src/drvn/cryptography_challenges/s02_c14_byte_at_a_time_ecb_decryption_harder.py
@@ -0,0 +1,11 @@
+"""
+Byte at a time ECB decryption (Harder)
+
+AES-128-ECB(random-prefix || attacker-controlled || target-bytes, random-key)
+"""
+
+# import drvn.cryptography.aes as aes
+
+
+def run_challenge():
+    pass
